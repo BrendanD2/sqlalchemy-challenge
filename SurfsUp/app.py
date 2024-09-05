02dc1bbbbf30f1175d586970c81ae824c29ee8ba
BrendanD2 <brendandoucette2021@gmail.com>
2024-09-05 18:29:28 -0400

    adding final updates

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -11,7 +11,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -44,3 +44,80 @@ def welcome():
 
 
 # Create our session (link) from Python to the DB
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    
+    # Query for last 12 months of data
+    last_date = '2017-08-23'
+    start_date = '2016-08-23'
+    results = session.query(measurement.date, measurement.prcp).filter(measurement.date <= last_date).filter(measurement.date >= start_date).all()
+    session.close()
+    
+    past_year = []
+    for date, prcp in results:
+        past_dict = {}
+        past_dict['Date'] = date
+        past_dict['precipitation'] = prcp
+        past_year.append(past_dict)
+    return jsonify(past_year)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results = session.query(station.station).all()
+    session.close()
+    
+    all_station = list(np.ravel(results))
+    
+    return jsonify(all_station)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    last_date = '2017-08-23'
+    start_date = '2016-08-23'
+
+    results = session.query(measurement.tobs, measurement.date).filter(measurement.date <= last_date).filter(measurement.date >= start_date).filter(measurement.station == 'USC00519281').all()
+    session.close()
+    
+    past_year = []
+    for tobs, date in results:
+        past_dict = {}
+        past_dict['Temp'] = tobs
+        past_dict['Date'] = date
+        past_year.append(past_dict)
+    return jsonify(past_year)
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    session = Session(engine)
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).all()
+    session.close()
+    temp_list = []
+    for min, avg, max in results:
+        temp_dict = {}
+        temp_dict['min'] = min
+        temp_dict['avg'] = avg
+        temp_dict['max'] = max
+        temp_list.append(temp_dict)
+    return jsonify(temp_list)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    session = Session(engine)
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date <= end).filter(measurement.date >= start).all()
+    session.close()
+    temp_list = []
+    for min, avg, max in results:
+        temp_dict = {}
+        temp_dict['min'] = min
+        temp_dict['avg'] = avg
+        temp_dict['max'] = max
+        temp_list.append(temp_dict)
+    return jsonify(temp_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+        
